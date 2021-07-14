27b0e4a3d7e51596b19e8c963bd9700531f60ef5
Infinity20998 <87230537+Infinity20998@users.noreply.github.com>
2021-07-14 17:15:13 +0530

    Update alive.py

--- userge/plugins/bot/alive.py
+++ userge/plugins/bot/alive.py
@@ -227,12 +227,11 @@ class Bot_Alive:
     @staticmethod
     def alive_info() -> str:
         alive_info_ = f"""
-<a href="https://t.me/TheEmptyVoid"><b>SHOTO-𝑿</a> is Up and Running.</b>
+<a href="https://t.me/TheEmptyVoid"><b>SHOTO-𝑿</a> is Up and Running!</b>
 
-    <b>〣 Python :</b>    <code>v{versions.__python_version__}</code>
-    <b>〣 Pyrogram :</b>    <code>v{versions.__pyro_version__}</code>
-    <b>〣 𝑿 :</b>    <code>v{get_version()}</code>
-
+🐍   <b>Python :</b>    <code>v{versions.__python_version__}</code>
+🔥   <b>Pyrogram :</b>    <code>v{versions.__pyro_version__}</code>
+🧬   <b>𝑿 :</b>    <code>v{get_version()}</code>
 <b>{Bot_Alive._get_mode()}</b>    <code>|</code>    🕔  <b>{userge.uptime}</b>
 """
         return alive_info_
